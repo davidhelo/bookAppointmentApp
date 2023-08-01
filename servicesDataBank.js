285577a4575df2c45b29fcb158f6eb7291863ad8
dahelo <dohernandez93@gmail.com>
2023-08-01 14:52:33 +0100

    add datra bank for local testing dynamic generation of services pages

---
+++ servicesDataBank.js
@@ -0,0 +1,25 @@
+const servicesBank = [
+    {
+        service: "FACIAL LIMPIEZA PROFUNDA",
+        summary: "Indicado para pieles , que presentan puntos negros.",
+        details: "Retarda el envejecimiento y la aparición de arrugas. Elimina impurezas y células muertas, ayuda a remover puntos negros y a regular el acné. Incluye: vapor, microdermoabrasión, alta frecuencia, masaje cuello y cabeza, presoterapia 25min. ",
+        price: 500,
+        image: ""
+    },
+    {
+        service: "HYDRAFACIAL",
+        summary: "Indicado para pieles apagadas con falta de hidratación y vitalidad.",
+        details: "Gracias a sus diferentes cabezales, combina en una misma sesión: limpieza, exfoliación y peeling. Un shot de antioxidantes para tu piel. Incluye: mascarilla plástica, presoterapia en piernas 30 min. Masaje en cuello.",
+        price: 700,
+        image: ""
+    },
+    {
+        service: "",
+        summary: "",
+        details: "",
+        price: 700,
+        image: ""
+    }
+];
+
+export default servicesBank;
