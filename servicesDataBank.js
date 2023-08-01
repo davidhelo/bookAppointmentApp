a163b5e1146d4d539296610765d83c568e5e3c51
dahelo <dohernandez93@gmail.com>
2023-08-01 19:38:27 +0100

    chore add services to databank

--- servicesDataBank.js
+++ servicesDataBank.js
@@ -1,25 +1,57 @@
-const servicesBank = [
-    {
-        service: "FACIAL LIMPIEZA PROFUNDA",
-        summary: "Indicado para pieles , que presentan puntos negros.",
-        details: "Retarda el envejecimiento y la aparición de arrugas. Elimina impurezas y células muertas, ayuda a remover puntos negros y a regular el acné. Incluye: vapor, microdermoabrasión, alta frecuencia, masaje cuello y cabeza, presoterapia 25min. ",
-        price: 500,
-        image: ""
-    },
-    {
-        service: "HYDRAFACIAL",
-        summary: "Indicado para pieles apagadas con falta de hidratación y vitalidad.",
-        details: "Gracias a sus diferentes cabezales, combina en una misma sesión: limpieza, exfoliación y peeling. Un shot de antioxidantes para tu piel. Incluye: mascarilla plástica, presoterapia en piernas 30 min. Masaje en cuello.",
-        price: 700,
-        image: ""
-    },
-    {
-        service: "",
-        summary: "",
-        details: "",
-        price: 700,
-        image: ""
-    }
-];
-
+const servicesBank = {
+    facialServices: [
+        {
+            service: "FACIAL LIMPIEZA PROFUNDA",
+            summary: "Indicado para pieles , que presentan puntos negros.",
+            details: "Retarda el envejecimiento y la aparición de arrugas. Elimina impurezas y células muertas, ayuda a remover puntos negros y a regular el acné. Incluye: vapor, microdermoabrasión, alta frecuencia, masaje cuello y cabeza, presoterapia 25 minutos. ",
+            price: 500,
+            image: ""
+        },
+        {
+            service: "HYDRAFACIAL",
+            summary: "Indicado para pieles apagadas con falta de hidratación y vitalidad.",
+            details: "Gracias a sus diferentes cabezales, combina en una misma sesión: limpieza, exfoliación y peeling. Un shot de antioxidantes para tu piel. Incluye: mascarilla plástica, presoterapia en piernas 30 min. Masaje en cuello. 90 minutos.",
+            price: 700,
+            image: ""
+        },
+        {
+            service: "FACIAL HIDRATANTE CON RADIOFRECUENCIA",
+            summary: "Ideal para pieles apagadas, que presentan primeras arrugas finas.",
+            details: "Una combinación de radiofrecuencia y un cocktail de vitaminas (ampolleta). 60 minutos.",
+            price: 550,
+            image: ""
+        },
+        {
+            service: "FACIAL DERMAPEN",
+            summary: "Ideal para pieles que desean mejorar la textura de su piel, disminuir cicatrices, apariencia de poros dilatados, desvanece arrugas, unifica el tono de la piel, aporta vitalidad y luminosidad.",
+            details: "Incluye: masaje en cuello, presoterapia, mascarilla plástica. 80 minutos",
+            price: 850,
+            image: ""
+        },
+        {
+            service: "LIFTING FACIAL CON BEM THERAPY",
+            summary: "Indicado para prevenir y tratar flacidez, efecto lifting, revitalizante y rellenador.",
+            details: "Incluye: Bem therapy, presoterapia, mascarilla anti-envejecimiento. 80 minutos. (ó 5 sesiones $2,200.00)",
+            price: 680,
+            image: ""
+        },
+        {
+            service: "BIO CONTORNO DE OJOS",
+            summary: "Indicado para mirada cansada, bolsas palpebrales, líneas de expresión (patas de gallo).",
+            details: "Incluye, mascarilla oxigenante, presoterapia en piernas. 40 minutos.",
+            price: 350,
+            image: ""
+        }
+    ],
+    depilationServices: [
+        {
+            service: "DEPILACION LASER TRI-DIODO",
+            summary: "La tecnología mas avanzada en el mercado en equipos de láser. Elimina el vello de forma definitiva en un 96% en 8-12 sesiones, seguro, eficaz, cómodo y con resultados duraderos. ",
+            details: "Procedimiento sin dolor. Permite tratar grandes superficies de piel, así como zonas sensibles.",
+            price: 0,
+            image: ""
+        }
+    ]
+};
+    
 export default servicesBank;
